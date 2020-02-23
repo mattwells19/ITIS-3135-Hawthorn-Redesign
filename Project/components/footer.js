2b2b349e6b0b8e61d090715d6328dbba31d90ce4
mattwells19 <mwells21@uncc.edu>
2020-02-23 15:12:08 -0500

    Toggle footer with jQuery

--- Project/components/footer.js
+++ Project/components/footer.js
@@ -90,13 +90,7 @@ document.getElementById("footer").innerHTML = `
 `;
 
 var handleFooterClick = () => {
-  var footerExtra = document.getElementById("collapseOne");
-  if (footerExtra.className.includes(" expanded")) {
-    footerExtra.className = footerExtra.className.replace(" expanded", "");
-  } else {
-    footerExtra.className += " expanded";
-    setTimeout(() => window.scrollTo(0, document.body.scrollHeight), 200);
-  }
+  $("#collapseOne").toggle("blind", 500);
 };
 
 document.getElementById("footerExtra").onclick = handleFooterClick;
