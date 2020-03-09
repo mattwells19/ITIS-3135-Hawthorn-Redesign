e1bd483061ef07d3335f06f02dde795ead74198c
mattwells19 <mwells21@uncc.edu>
2020-03-09 15:29:55 -0400

    Start to activity 9

---
+++ Exercise/Activity9/Wells-Activity9.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+        // get the image URL and caption for each image
+
+
+        // preload the image for each link
+
+        // set up the event handlers for each link
+
+
+            // cancel the default action of each link
+
+
+    // move the focus to the first link
+
+}); // end ready
