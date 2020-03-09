b58bf12221b39750a9da7d37d0d9bc849c98d08a
mattwells19 <mwells21@uncc.edu>
2020-03-09 19:21:32 -0400

    Correctly preloading images

--- Exercise/Activity9/Wells-Activity9.js
+++ Exercise/Activity9/Wells-Activity9.js
@@ -2,16 +2,22 @@ $(function () {
 
     // preload the image for each link
     // set up the event handlers for each link
+    var images = [];
+    var captions = [];
     $("a").each((i, val) => {
-        $('<img/>')[0].src = val.href
-        $(val).click(handleLinkClick);
+        images.push($('<img>').attr({
+            src: val.href,
+            id: "image",
+        }));
+        captions.push(val.title);
+        $(val).click((evt) => handleLinkClick(evt, i));
     });
 
     // Click event callback
-    function handleLinkClick(evt) {
+    function handleLinkClick(evt, i) {
         evt.preventDefault();
-        $("#image").attr("src", evt.target.href);
-        $("#caption").text(evt.target.title);
+        $("#image").replaceWith(images[i]);
+        $("#caption").text(captions[i]);
     };
 
     $("a:eq(1)").focus()
