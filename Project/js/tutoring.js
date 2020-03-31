e7e61f1769c27d4154e541db807ada623f89effb
mattwells19 <mwells21@uncc.edu>
2020-03-30 23:37:09 -0400

    Added animatescroll plugin

--- Project/js/tutoring.js
+++ Project/js/tutoring.js
@@ -2,4 +2,9 @@ $(function() {
   $(".tutor-hours").click(event => {
     $(event.currentTarget).toggleClass("open");
   });
+
+  $(".about-tutoring").on("click", "a", (e) => {
+    e.preventDefault();
+    $(e.target.hash).animatescroll({scrollSpeed:100,easing:'easeOutQuint'});
+  });
 });
