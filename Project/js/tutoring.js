3ae435a0f416eaaad481975fd62ce120e832a76d
mattwells19 <mwells21@uncc.edu>
2020-04-05 20:54:39 -0400

    Updates for assignment 3

--- Project/js/tutoring.js
+++ Project/js/tutoring.js
@@ -5,7 +5,7 @@ $(function () {
   });
 
   // Uses the animatescroll JQuery plugin when a tutor link is clicked
-  $(".about-tutoring").on("click", "a", (e) => {
+  $(".about-tutoring").on("click", "li a", (e) => {
     e.preventDefault();
     $(e.target.hash).animatescroll({ scrollSpeed: 100, easing: 'easeOutQuint' });
   });
