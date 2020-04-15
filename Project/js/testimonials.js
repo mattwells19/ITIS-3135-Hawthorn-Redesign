11c8026ec3ddb7044bc1777c9ce7b79756d67d14
mattwells19 <mwells21@uncc.edu>
2020-04-15 02:06:37 -0400

    Ajax content for tutoring and testimonials

---
+++ Project/js/testimonials.js
@@ -0,0 +1,34 @@
+$(function () {
+    $.ajax({
+        type: "get",
+        url: "/data/testimonials.json",
+        timeout: 10000,
+        error: function (xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function (data) {
+            $("#testimonials").html("");
+            for (const test of data.testimonials) {
+                $("#testimonials").append(getTestimonialCard(test));
+            }
+        }
+    });
+})
+
+const getTestimonialCard = (testData) => {
+    return `<!-- Beginning of testimonial card -->
+    <div class="testimonial-card">
+      <img src="${testData.image}" alt="Testimonial Image" height=300 />
+      <div class="testimonial-card-content">
+        <h3>${testData.name}</h3>
+        <h4>${testData.year}, ${testData.major}</h4>
+        <br />
+        <strong>What they had to say:</strong>
+        <p>
+          ${testData.message}
+        </p>
+      </div>
+    </div>
+    <!-- End of testimonial card -->`
+}
